4f2dfbaa8ce7891364a5f37a2a8d77bf0eb9fe6f
vivraman <vivraman@gmail.com>
2015-06-05 22:21:36 -0700

    added new solve for 23, not yet finished

--- src/eulerjava/Euler023.java
+++ src/eulerjava/Euler023.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public class Euler023 {
 
     ArrayList<Integer> abundants;
+    boolean[] sums;
+    final int UPPERBOUND = 28123;
 
     public Euler023() {
         long time = System.currentTimeMillis();
@@ -17,12 +19,29 @@ public class Euler023 {
 
     public long solve() {
         abundants = new ArrayList();
+        sums = new boolean[UPPERBOUND + 1];
         long answer = 0;
-        for (int i = 1; i <= 28123; i++) {
+        for (int i = 1; i < UPPERBOUND; i++) {
             if (isAbundant(i)) {
                 abundants.add(i);
             }
-            answer += sumOfAbundants(i);
+            //answer += sumOfAbundants(i);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < abundants.size(); i++) {
+            for (int j = 0; j < abundants.size(); j++) {
+                if ((sum = abundants.get(i) + abundants.get(j)) < UPPERBOUND) {
+                    sums[sum] = true;
+                } else {
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < sums.length - 1; i++) {
+            if(!sums[i]){
+                System.out.println(i);
+                answer+=i;}
         }
         return answer;
     }
@@ -38,38 +57,20 @@ public class Euler023 {
         return sum > num;
     }
 
-    public int sumOfAbundants(int i) {
-        /*int sum = 0;
-        for (int j = 0; j < abundants.size(); j++) {
-            if (abundants.get(j) > i) {
-                break;
-            } else {
-                for (int k = 0; k < abundants.size(); k++) {
-                    if (abundants.get(k) > i) {
-                        break;
-                    } else {
-                        if((sum = abundants.get(j) + abundants.get(k)) == i){
-                            
-                        }
-                    }
-                }
-            }
-        }
-        return i;*/
-        int start = 0, end = abundants.size() - 1, sum = 0;
-        while (end > start) {
-            if(abundants.get(start) << 1 == i){
-                return 0;
-            }
-            if ((sum = abundants.get(start) + abundants.get(end)) == i) {
-                return 0;
-            } else if (sum > i) {
-                end--;
-            } else {
-                start++;
-            }
-        }
-        System.out.println(i);
-        return i;
-    }
+    /*
+     public int sumOfAbundants(int i) {
+     int start = 0, end = abundants.size() - 1, sum = 0;
+     while (end >= start) {
+     if ((sum = abundants.get(start) + abundants.get(end)) == i) {
+     System.out.println(i + "=" + abundants.get(start) + "+" + abundants.get(end));
+     return 0;
+     } else if (sum > i) {
+     end--;
+     } else {
+     start++;
+     }
+     }
+     System.out.println(i + " adding to sum");
+     return i;
+     }*/
 }

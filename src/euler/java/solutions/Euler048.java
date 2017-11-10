29102a0736264eb6af0911b9cfb11f1428b2417e
Vivek Raman <vivraman@gmail.com>
2017-11-10 08:39:07 -0500

    finished 48

---
+++ src/euler/java/solutions/Euler048.java
@@ -0,0 +1,29 @@
+package euler.java.solutions;
+
+import java.math.BigInteger;
+
+/**
+ * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ *
+ * Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+ */
+public class Euler048 extends EulerProblem {
+
+    private static final int LIMIT = 1000;
+
+    /**
+     * Use BigInteger to power and sum as necessary.
+     *
+     * @return solution to Problem 48
+     */
+    @Override
+    protected String solve() {
+        BigInteger total = new BigInteger(1 + "");
+        for (int i = 2; i <= LIMIT; i++) {
+            total = total.add(new BigInteger(i + "").pow(i));
+        }
+
+        String answer = total.toString();
+        return answer.substring(answer.length() - 10);
+    }
+}
